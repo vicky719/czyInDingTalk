407e3447a7fa7a986a94dd2f823646f8ee7d2c9c
WJ <Wangjie@ifugle.com>
2018-04-02 10:38:14 +0800

    新增：支付宝rap-vue-ui

--- src/components.js
+++ src/components.js
@@ -1,4 +1,14 @@
 import {czyFooter} from './apps/public'
+import {InputAutoclear, SearchAutoclear, Swipe, Select, Alert, Confirm, Mtswipe, MtswipeItem, RequireFor} from 'rap-vue-ui'
 export default (Vue) => {
   Vue.component('czy-footer', czyFooter)
+  Vue.component(InputAutoclear.name, InputAutoclear)
+  Vue.component(SearchAutoclear.name, SearchAutoclear)
+  Vue.component(Swipe.name, Swipe)
+  Vue.component(Select.name, Select)
+  Vue.component(Alert.name, Alert)
+  Vue.component(Confirm.name, Confirm)
+  Vue.component(Mtswipe.name, Mtswipe)
+  Vue.component(MtswipeItem.name, MtswipeItem)
+  Vue.directive(RequireFor.name, RequireFor)
 }
